cd0cc996962f0ad7cb53f260da96ea08f29388fa
sami4thewin <samiomarmohammed@gmail.com>
2017-03-01 11:41:52 -0500

    Working on the Prop type validations

--- components/Product.js
+++ components/Product.js
@@ -4,17 +4,24 @@ import ReactDOM from 'react-dom';
 class Product extends React.Component {
   render() {
     return (
-      <h1>{this.props.name}</h1>
-      <h1>{this.props.producer}</h1>
-      <h1>{this.props.hasWatermark}</h1>
-      <h1>{this.props.color}</h1>
-      <h1>{this.props.weight}</h1>
+      <div>
+        <h1>{this.props.name}</h1>
+        <h1>{this.props.producer}</h1>
+        <h1>{this.props.hasWatermark}</h1>
+        <h1>{this.props.color}</h1>
+        <h1>{this.props.weight}</h1>
+      </div>
     )
   }
 }
 
 Product.defaultProps = {
-  
+  hasWatermark: false,
+}
+
+Product.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  producer: React.PropTypes.string
 }
 
 
