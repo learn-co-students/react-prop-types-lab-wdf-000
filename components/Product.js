6431d2cd8619bbddcd6138d813b082d07e3aa11b
sami4thewin <samiomarmohammed@gmail.com>
2017-03-01 12:26:35 -0500

    Finished

--- components/Product.js
+++ components/Product.js
@@ -26,14 +26,23 @@ Product.propTypes = {
   hasWatermark: React.PropTypes.bool,
   color: React.PropTypes.oneOf(['white', 'eggshell-white',
   'salmon']).isRequired,
-  weight: function(props, propName, componentName) {
-    debugger;
-    // if (typeof props[propName] !== "number" || props[propName] > 300 || props[propName] < 80) {
-    //   return new Error("Sorry")
-    // } else {
-    //   return null
-    // }
-  }
+  weight: (props, propName) => {
+    const weight = props[propName];
+
+    if (weight === undefined) {
+      return new Error('The `weight` prop is required.');
+    }
+
+    if (isNaN(weight)) {
+      return new Error('The `weight` prop is not a number.');
+    }
+
+    const isValidWeight = weight > 80 && weight < 300;
+
+    if (!isValidWeight) {
+      return new Error('The `weight` prop should range between 80 and 300.');
+    }
+  },
 }
 
 
